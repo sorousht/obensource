05d62ed8b45ca56a49a54524ef440fb1188275ec
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2018-12-21 00:17:50 +0330

    Customize the card

--- bin/card.js
+++ bin/card.js
@@ -9,38 +9,25 @@ const boxen = require('boxen')
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
+  borderStyle: 'round',
+  float: 'center',
 }
+
+const newline = '\n';
 
 // Text + chalk definitions
-const data = {
-  name: chalk.white('Ben Michel /'),
-  handle: chalk.cyan('obensource'),
-  work: chalk.white('Developer Relations Advocate at InVision'),
-  twitter: chalk.cyan('https://twitter.com/obensource'),
-  github: chalk.cyan('https://github.com/obensource'),
-  linkedin: chalk.cyan('https://linkedin.com/in/ben-michel-044a3985'),
-  web: chalk.cyan('http://benmichel.com'),
-  npx: chalk.white('npx obensource'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:')
-}
-
-// Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const data = [
+  chalk.bold.white('Soroush Torkzadeh'),
+  chalk.white('Front-end JavaScript Developer'),
+  chalk.white(newline),
+  chalk.dim.white('    Website:') + chalk.bold.white(' http://sorousht.com'),
+  chalk.dim.white('     GitHub:') + chalk.bold.white(' https://github.com/sorousht'),
+  chalk.dim.white('   Linkedin:') + chalk.bold.white(' https://www.linkedin.com/in/sorousht/'),
+  chalk.white(newline),
+  chalk.green('$ npx sorousht'),
+];
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = data.join(newline);
 
 console.log(chalk.green(boxen(output, options)))
